53456031192776e94de58c8f06da1e649a4aface
Biletnikoff <bogeresanders@gmail.com>
2016-07-09 15:43:55 -0700

    server init

--- routes.js
+++ routes.js
@@ -2,19 +2,14 @@ var carRouter = require('express').Router();
 var userController = require('./controllers/userController.js');
 var favoriteController = require('./controllers/favoriteController.js');
 
-// module.exports = function(app) {
-//   app.post('/api/users', userController.createOne);
-//   app.post('/api/users/login', userController.verifyLogin);
-// }
-
-carRouter.route('/users') 
+carRouter.route('/users')
   .post(userController.createOne)
 
 carRouter.route('/users/login')
   .post(userController.verifyLogin)
 
 carRouter.route('/users/FbLogin')
-  .post(userController.verifyFBLogin)  
+  .post(userController.verifyFBLogin)
 
 carRouter.route('/users/getID')
   .post(userController.getUserID)
